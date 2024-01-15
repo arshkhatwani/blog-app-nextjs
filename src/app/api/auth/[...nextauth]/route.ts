89384c53ab592af08945653166adfc367731f93e
arshkhatwani <arsh.khatwani@gmail.com>
2024-01-15 16:23:12 +0530

    Add nextauth with dummy users

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,35 @@
+import NextAuth, { AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import users from "@/app/constants/users";
+
+const authOptions: AuthOptions = {
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: {
+                    label: "Email",
+                    type: "text",
+                    placeholder: "Enter your email",
+                },
+                password: { label: "Password", type: "password" },
+            },
+            async authorize(credentials) {
+                const email = credentials?.email;
+                const password = credentials?.password;
+
+                if (!email || !password) return null;
+
+                const user = users.find((e) => e.email === email);
+                if (user?.password === password)
+                    return { id: user.id, email: user.email, name: user.name };
+
+                return null;
+            },
+        }),
+    ],
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
---
+++ src/app/constants/users.ts
@@ -0,0 +1,16 @@
+const users = [
+    {
+        id: "1",
+        name: "John",
+        email: "john@example.com",
+        password: "sample",
+    },
+    {
+        id: "2",
+        name: "Mary",
+        email: "mary@example.com",
+        password: "sample2",
+    },
+];
+
+export default users;
