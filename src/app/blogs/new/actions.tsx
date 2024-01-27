c1b85688a48e950a745fe03041ef89d023c2d7be
arshkhatwani <arsh.khatwani@gmail.com>
2024-01-27 19:53:17 +0530

    Format code

--- src/app/blogs/new/actions.tsx
+++ src/app/blogs/new/actions.tsx
@@ -6,7 +6,7 @@ import prisma from "@/db";
 export async function createBlog(data: { title: string; body: any }) {
     const { title, body } = data;
     const user = await getUserDetails();
-    if(!user) return;
+    if (!user) return;
     const userId = user?.id as string;
 
     const newBlog = await prisma.blog.create({
